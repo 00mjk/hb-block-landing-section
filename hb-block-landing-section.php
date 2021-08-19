cd022847ef37607430bbe8bc2f9f469d9ddc6a70
Jefferson <me@jeffersonreal.com>
2021-08-19 23:38:10 +0100

    doc cleanup

--- hb-block-landing-section.php
+++ hb-block-landing-section.php
@@ -1,26 +1,22 @@
 <?php
 /**
  * Plugin Name:       Hb Block Landing Section
- * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Description:       A full width grid template for landing page sections.
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Jefferson Real
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       hb-block-landing-section
- *
- * @package           create-block
  */
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued
  * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
  */
-function create_block_hb_block_landing_section_block_init() {
+function hb_block_landing_section_init() {
 	register_block_type( __DIR__ );
 }
-add_action( 'init', 'create_block_hb_block_landing_section_block_init' );
+add_action( 'init', 'hb_block_landing_section_init' );
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Hb Block Landing Section
+
+Author:            Jefferson Real
+Tags:              block, landing page, full width
+Tested up to:      5.8.0
+Stable tag:        0.1.0
+License:           GPL-2.0-or-later
+License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+
+
+## Description
+
+A full width grid template for landing page sections.
+
+The grid template provides three fluid sets of tracks which provide a means of
+displaying content at 1:1, 3:2 and 16:9 aspect ratios plus full width (when this
+is larger than 16:9). The tracks have minimum widths based on the widest available
+'normal' monitor aspect ratio with a resolution of 3440x1440 aka ultrawide. The
+central tracks creating the 1:1 space will never be narrower than 40% of the browser
+width to prevent over shrinking when the browser is squished vertically.
+
+## Installation
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload the plugin files to the `/wp-content/plugins/hb-block-landing-section` directory, or install the plugin through the WordPress plugins screen directly.
+1. Activate the plugin through the 'Plugins' screen in WordPress
+
+
+### Changelog
+
+**0.1.0**
+* Release
